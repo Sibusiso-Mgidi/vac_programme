e28314f86b92b55401eee45be0620981e70a5fc5
Sibusiso-Mgidi <mgiditercy@gmail.com>
2021-07-26 16:57:12 +0200

    updated student class

--- intro_day3.py
+++ intro_day3.py
@@ -1,42 +1,19 @@
-"""
-Student
- - First name
- - Last name
- - Student number
-
-Course
- - Course name
- - Course code  
- 
-Assessment
- - Questions 10
- - Marks
- - Date
- - Lecturer
- - Answers 10
-
- Exam: Student, Course and Assessment
-
-
-"""
 
 class Student(object):
     """
     This is a blueprint for the student demographic data
     """
     
-    def __init__(self, student_firstname, student_lastname,student_number):
-        self.student_firstname =  student_firstname
-        self.student_lastname = student_lastname
+    def __init__(self, firstname, lastname, student_number):
+        self.student_firstname =  firstname
+        self.student_lastname = lastname
         self.student_number = int(student_number)
 
-
-    def print_demographic(self):
-        """
-        This is the function that returns students demographic data
-        """
+    def __repr__(self):
 
         return "{} {}".format(self.student_firstname, self.student_lastname)
+
+
     
 class Course(object):
     """
@@ -69,17 +46,8 @@ class Course(object):
         """
         pass
 
-
-# student_1 = Student("Thapelo", "Seletisha", "1234567")
-# student_demo_1 = student_1.print_demographic()
-# print(student_demo_1)
+student_1 = Student("Thapelo", "Seletisha", "1234567")
+print(student_1)
 
 
-# student_2 = Student("Teboho", "Molise", "24681012")
-# student_demo_2 = student_2.print_demographic()
-# print(student_demo_2)
 
-
-course_object = Course("Maths", "MATH123")
-c =course_object.print_information()
-print(c)
