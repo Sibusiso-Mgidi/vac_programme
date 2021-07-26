36916e9143c6e52a261f3f31bd017fa7aab3361d
Sibusiso-Mgidi <mgiditercy@gmail.com>
2021-07-26 17:07:39 +0200

    updated course class

--- intro_day3.py
+++ intro_day3.py
@@ -13,41 +13,22 @@ class Student(object):
 
         return "{} {}".format(self.student_firstname, self.student_lastname)
 
-
-    
+  
 class Course(object):
     """
     This is ablueprint for course information
     """
-    def __init__(self, course_name, course_code):
+    def __init__(self, course_name, course_code, lecturer_name):
         self.course_name = course_name
         self.course_code = course_code
-        self.assessments = list()
+        self.lecturer_name = lecturer_name
 
-    def print_information(self):
-        """
-        This function returns course information
-        """
-        student_info = Student("Thapelo", "Seletisha", "1234567")
-        student_demo_1 = student_info.print_demographic()
-
+    def __repr__(self):
+        return "{} {}".format(self.course_name, self.lecturer_name)
     
+student_object = Student("Thapelo", "Seletisha", "1234567")
+course_object = Course("Linear Algebra", "MATH2025", "Zellenyik")
 
-        return "{}".format( student_demo_1)
-
-    def add_assesment(self, lecturer, date):
-        """
-        This allows lecturers to add assessments
-
-        arg: 
-            - lectuerer: this is lecturers name
-            - date: this is the date  in format DD/MM/YYYY
-        return: this function ....
-        """
-        pass
-
-student_1 = Student("Thapelo", "Seletisha", "1234567")
-print(student_1)
 
 
 
